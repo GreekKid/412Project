9d1cdc68d540a376d5e2e79998a888c01d3a84f5
McGarvelcuddy <mcgarvelcuddy@gmail.com>
2020-02-16 15:41:36 -0500

    Added JavaDocs and spruced up the functions to make sense

--- IST412_ModuleDesign/src/com/atzhealthcare/PrescriptionHandling.java
+++ IST412_ModuleDesign/src/com/atzhealthcare/PrescriptionHandling.java
@@ -20,16 +20,34 @@ public class PrescriptionHandling {
         this.restrictedMedications = Arrays.asList("Adderall", "Oxy", "Something Else");
     }
     
-    public void check(String checkedMed, Integer ID){
+    
+    /**
+     * Confirms if the medication being checked is in restrictedMedications, and if so, checks the state medical database and returns any records the patient has.  
+     * Calls databaseParse() to parse what is gathered into JSON.
+     * @param checkedMed String specifying the name of the medication that is intended to be prescribed.
+     * @param ID ID of the patient that is going to be receiving the medication in order to check the database for them.
+     * @return Returns either null or JSON of a database call.
+     */
+    public ArrayList<String> check(String checkedMed, Integer ID){
+        ArrayList<String> dbRes = new ArrayList<>();
         if (restrictedMedications.contains(checkedMed)){
             //do API or other web check to check medical databases for this person associated with the ID  
-            ArrayList<String> databaseResults = new ArrayList<>();
-            databaseParse(databaseResults);
+            dbRes = databaseParse(dbRes);
+            return dbRes;
         }
-        
+        return dbRes;
     }
     
-    private void databaseParse(ArrayList<String> databaseResults){
-        
+    
+    /**
+     * Parses and formats database pull in check() into JSON.  
+     * 
+     * Will likely be multiple functions later on.
+     * @param databaseResults Takes in the JSON to be parsed in whatever way is needed.
+     * @return Appropriately formatted JSON.
+     */
+    private ArrayList<String> databaseParse(ArrayList<String> databaseResults){
+        databaseResults = databaseResults;  //take whatever is needed.  May need to be multiple separate functions
+        return databaseResults;
     }
 }
