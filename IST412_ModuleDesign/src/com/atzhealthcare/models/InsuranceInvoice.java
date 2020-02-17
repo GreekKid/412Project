ab9d2f2f33d924169098de4896ebf633552d020c
roscoeturner_ <ginganinjarules@gmail.com>
2020-02-16 21:51:56 -0500

    altered InsuranceInvoice

--- IST412_ModuleDesign/src/com/atzhealthcare/models/InsuranceInvoice.java
+++ IST412_ModuleDesign/src/com/atzhealthcare/models/InsuranceInvoice.java
@@ -4,21 +4,26 @@ public class InsuranceInvoice {
 
     private double insuranceCopay;
     private Appointment appt;
-    
+    private String providerId;
+
     /**
-     *Default constructor to generate an empty InsuranceInvoice object
+     * Default constructor to generate an empty InsuranceInvoice object
      */
-    public InsuranceInvoice(){}
+    public InsuranceInvoice() {
+    }
+
     /**
      * Creates a new invoice instance populated with relevant patient data
      *
      * @param insuranceCopay amount of bill insurance will pay.
      * @param appt Appointment object holding data relevant to patient's
      * appointment
+     * @param providerId id of insurance provider held within database
      */
-    public InsuranceInvoice(double insuranceCopay, Appointment appt) {
+    public InsuranceInvoice(double insuranceCopay, Appointment appt, String providerId) {
         this.insuranceCopay = insuranceCopay;
         this.appt = appt;
+        this.providerId = providerId;
     }
 
     /**
